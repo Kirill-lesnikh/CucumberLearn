df470e1823140deec34dcdf52570207a0207c1e1
Pufi <kirilllesnikh@gmail.com>
2020-08-21 17:45:01 +0300

    21.08

--- config/open_browser.rb
+++ config/open_browser.rb
@@ -1,10 +1,14 @@
 require '../features/support/has_browser'
 require 'watir'
+require_relative '../features/pages/login_page'
+require_relative '../features/pages/base_page'
 
 include HasBrowser
-
+a = BasePage.new
+b = LoginPage.new
 start_browser_session
 $new_browser.goto('dev-my.allego.com/')
+b.log_in('creator')
 z = gets
 
 
--- features/support/has_browser.rb
+++ features/support/has_browser.rb
@@ -9,12 +9,14 @@ module HasBrowser
   end
 
   def start_browser_session
-    profile = Selenium::WebDriver::Firefox::Profile.new
-    profile['media.navigator.permission.disabled'] = true
-    profile['media.navigator.streams.fake'] = true
+    #  profile = Selenium::WebDriver::Firefox::Profile.new
+    #  profile['media.navigator.permission.disabled'] = true
+    #  profile['media.navigator.streams.fake'] = true
 
     # Starting a new browser
-    $new_browser = Watir::Browser.new :firefox, :profile => profile
+    $new_browser = Watir::Browser.new :chrome, args: %w(--use-fake-ui-for-media-stream --use-fake-device-for-media-stream)
+    #Watir::Browser.new :firefox, :profile => profile
+    $new_browser.window.maximize
   end
 
   def change_tab
@@ -24,4 +26,6 @@ module HasBrowser
   def get_browser_windows_size
     browser.windows.size
   end
+
+
 end
